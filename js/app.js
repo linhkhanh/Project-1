222aa80f39a01ae7ba3a2f737bc8f4fe2545f629
Linh Le <linhle@Hieus-MacBook-Air.local>
2020-04-24 10:58:36 +0800

    organize code

--- js/app.js
+++ js/app.js
@@ -11,234 +11,166 @@ const house = ['https://i.pinimg.com/originals/34/30/2d/34302d7a184289404e2dff1f
     'https://www.pngkey.com/png/detail/61-619052_house-cartoon-png-cartoon-house-transparent-png.png',
 ];
 
-/////function create array
-
-const createArr = (arr, items) => {
-    const subArr = [];
-    for(let i = 0; i < 2; i++) {
-        for(let j = 0; j < items/2; j++) {
-            subArr.push(arr[j]);
-        }
-    }
-    return subArr;
-}
-const houseLv1 = createArr(house, 4);
-const houseLv2 = createArr(house, 6);
-const houseLv3 = createArr(house, 8);
-const houseLv4 = createArr(house, 10);
-const houseLv5 = createArr(house, 12);
-const houseLv6 = createArr(house, 14);
-const houseLv7 = createArr(house, 16);
 
 /////// function logic 
-
-const createMenu = () => {
-    const $container = $('<div>').addClass('container');
-    $('.body-game').append($container);
-    for (let i = 0; i < menu.length; i++) {
-        const $div = $('<div>')
-            .addClass('square')
-            .append($('<p>').html(menu[i]));
-        $('.container').append($div);
-    }
-    $('.square').on('click', createLevel1);
-}
-
-const clearMenu = (level) => {
-    $('.body-game').empty();
-    createNav(level);
-    $('.body-game').append($('<div>').addClass('container'));
-    $('.home').on('click', backToMenu);
-}
-
-const createNav = (level) => {
-    $('.body-game').append($('<h2>').html(level));
-    const $div = $('<div>').addClass('result')
-    const $left = $('<div>').addClass('left');
-    const $right = $('<div>').addClass('right');
-    $div.append($left, $right);
-    $left.append($('<button>')
-        .html('home')
-        .addClass('home'),
-        $('<p>')
-            .addClass('again')
-            .html('again'),
-        $('<p>')
-            .addClass('quantity')
-            .html('?')
-    );
-    $right.append($('<button>')
-        .html('Next')
-        .addClass('next'),
-        $('<button>')
-            .html('Retry')
-            .addClass('retry'));
-    $('.body-game').append($div);
-}
-
-const addItem = (row, num) => {
-    for (let i = 0; i < row; i++) {
-        const $div = $('<div>').addClass('row');
-        for (let j = 0; j < num; j++) {
-            $div.append($('<div>')
-                .addClass('item'))
+const App = {
+    createArr: (arr, items) => {
+        const subArr = [];
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < items / 2; j++) {
+                subArr.push(arr[j]);
+            }
         }
-        $('.container').append($div);
+        return subArr;
+    },
+    createLevel: (level, row1, item1, row2, item2, arr, nextLevel) => {
+        UI.clearMenu(level);
+        UI.addItem(row1, item1);
+        UI.addItem(row2, item2);
+        UI.setBackgroundOfItem(arr);
+        UI.turnOverImg();
+
+        $('.item').on('click', EventHandler.onLCickDisplayFaceUp);
+        $('.again').on('click', () => {
+            EventHandler.onClickSeeImagesAgain(arr);
+        });
+        $('.next').on('click', () => {
+            EventHandler.onClickGoToNextLevel(nextLevel)
+        })
     }
 }
-
-const setBackgroundOfItem = (arr) => {
-    const $item = $('.item');
-    for (let i = 0; i < $item.length; i++) {
-        $item.eq(i)
-        .css('background-image', `url(${arr[i]})`)
-        .attr('id', i)
+//////////////UI
+const UI = {
+    createMenu: () => {
+        const $container = $('<div>').addClass('container');
+        $('.body-game').append($container);
+        for (let i = 0; i < menu.length; i++) {
+            const $div = $('<div>')
+                .addClass('square')
+                .append($('<p>').html(menu[i]));
+            $('.container').append($div);
+        }
+        $('.square').on('click', level1.level);
+    },
+    clearMenu: (level) => {
+        $('.body-game').empty();
+        UI.createNav(level);
+        $('.body-game').append($('<div>').addClass('container'));
+        $('.home').on('click', EventHandler.onClickBackToMenu);
+    },
+    createNav: (level) => {
+        $('.body-game').append($('<h2>').html(level));
+        const $div = $('<div>').addClass('result')
+        const $left = $('<div>').addClass('left');
+        const $right = $('<div>').addClass('right');
+        $div.append($left, $right);
+        $left.append($('<button>')
+            .html('home')
+            .addClass('home'),
+            $('<p>')
+                .addClass('again')
+                .html('again'),
+            $('<p>')
+                .addClass('quantity')
+                .html('?')
+        );
+        $right.append($('<button>')
+            .html('Next')
+            .addClass('next'),
+            $('<button>')
+                .html('Retry')
+                .addClass('retry'));
+        $('.body-game').append($div);
+    },
+    addItem: (row, num) => {
+        for (let i = 0; i < row; i++) {
+            const $div = $('<div>').addClass('row');
+            for (let j = 0; j < num; j++) {
+                $div.append($('<div>')
+                    .addClass('item'))
+            }
+            $('.container').append($div);
+        }
+    },
+    setBackgroundOfItem: (arr) => {
+        const $item = $('.item');
+        for (let i = 0; i < $item.length; i++) {
+            $item.eq(i)
+                .css('background-image', `url(${arr[i]})`)
+                .attr('id', i)
+        }
+    },
+    turnOverImg: () => {
+        setTimeout(() => {
+            $('.item').css('background-image', `url(${wall})`)
+        }, 3000)
     }
-}
-
-const turnOverImg = () => {
-    setTimeout(() => {
-        $('.item').css('background-image', `url(${wall})`)
-    }, 3000)
-}
-
-const displayFaceUp = (event) => {
-    const $target = $(event.currentTarget);
-    let index = $target.attr('id');
-    let h2 = $('h2').html();
-
-    if (h2 === 'Level 1') {
-        $target.css('background-image', `url(${houseLv1[index]})`);
-    } else if (h2 === 'Level 2') {
-        $target.css('background-image', `url(${houseLv2[index]})`);
-    } else if (h2 === 'Level 3') {
-        $target.css('background-image', `url(${houseLv3[index]})`);
-    } else if (h2 === 'Level 4') {
-        $target.css('background-image', `url(${houseLv4[index]})`);
-    } else if (h2 === 'Level 5') {
-        $target.css('background-image', `url(${houseLv5[index]})`);
-    } else if (h2 === 'Level 6') {
-        $target.css('background-image', `url(${houseLv6[index]})`);
-    } else {
-        $target.css('background-image', `url(${houseLv7[index]})`);
-    }  
 }
 //////////////////eventhandler
-const createLevel1 = () => {
-    clearMenu('Level 1');
-    addItem(2, 2);
-    setBackgroundOfItem(houseLv1);
-    turnOverImg();
+const EventHandler = {
+    onClickBackToMenu: () => {
+        $('.body-game').empty();
+        UI.createMenu();
+    },
+    onClickSeeImagesAgain: (arr) => {
+        UI.setBackgroundOfItem(arr);
+        UI.turnOverImg();
+    },
+    onClickGoToNextLevel: (nextLevel) => {
+        nextLevel()
+    },
+    onLCickDisplayFaceUp: (event) => {
+        const $target = $(event.currentTarget);
+        let index = $target.attr('id');
+        let h2 = $('h2').html();
 
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel2);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv1);
-        turnOverImg();
-    });
+        if (h2 === 'Level 1') {
+            $target.css('background-image', `url(${level1.arr[index]})`);
+        } else if (h2 === 'Level 2') {
+            $target.css('background-image', `url(${level2.arr[index]})`);
+        } else if (h2 === 'Level 3') {
+            $target.css('background-image', `url(${level3.arr[index]})`);
+        } else if (h2 === 'Level 4') {
+            $target.css('background-image', `url(${level4.arr[index]})`);
+        } else if (h2 === 'Level 5') {
+            $target.css('background-image', `url(${level5.arr[index]})`);
+        } else if (h2 === 'Level 6') {
+            $target.css('background-image', `url(${level6.arr[index]})`);
+        } else {
+            $target.css('background-image', `url(${level7.arr[index]})`);
+        }
+    }
 }
-
-const backToMenu = () => {
-    $('.body-game').empty();
-    createMenu();
+///////////////////////
+class Level {
+    constructor(arr, level) {
+        this.arr = arr,
+        this.level = level
+    }
 }
-
-const createLevel2 = () => {
-    clearMenu('Level 2');
-    addItem(2, 3);
-    setBackgroundOfItem(houseLv2);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel3);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv2);
-        turnOverImg();
-    });
-}
-
-
-const createLevel3 = () => {
-    clearMenu('Level 3');
-    addItem(2, 3);
-    addItem(1, 2);
-    setBackgroundOfItem(houseLv3);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel4);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv3);
-        turnOverImg();
-    });
-}
-
-const createLevel4 = () => {
-    clearMenu('Level 4');
-    addItem(3, 3);
-    addItem(1, 1);
-    setBackgroundOfItem(houseLv4);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel5);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv4);
-        turnOverImg();
-    });
-}
-
-const createLevel5 = () => {
-    clearMenu('Level 5');
-    addItem(4, 3);
-    setBackgroundOfItem(houseLv5);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel6);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv5);
-        turnOverImg();
-    });
-}
-
-const createLevel6 = () => {
-    clearMenu('Level 6');
-    addItem(3, 4);
-    addItem(1, 2);
-    setBackgroundOfItem(houseLv6);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.next').on('click', createLevel7);
-
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv6);
-        turnOverImg();
-    });
-}
-
-const createLevel7 = () => {
-    clearMenu('Level 7');
-    addItem(4, 4);
-    setBackgroundOfItem(houseLv7);
-    turnOverImg();
-
-    $('.item').on('click', displayFaceUp);
-    $('.again').on('click', () => {
-        setBackgroundOfItem(houseLv7);
-        turnOverImg();
-    });
-}
-
-//img///
+let level1 = new Level(App.createArr(house, 4), () => {
+    App.createLevel('Level 1', 2, 2, 0, 0, App.createArr(house, 4), level2.level);
+} );
+let level2 = new Level(App.createArr(house, 6), () => {
+    App.createLevel('Level 2', 2, 3, 0, 0, App.createArr(house, 6), level3.level)
+});
+let level3 = new Level(App.createArr(house, 8), () => {
+    App.createLevel('Level 3', 2, 3, 1, 2, App.createArr(house, 8), level4.level)
+});
+let level4 = new Level(App.createArr(house, 10), () => {
+    App.createLevel('Level 4', 3, 3, 1, 1, App.createArr(house, 10), level5.level)
+});
+let level5 = new Level(App.createArr(house, 12), () => {
+    App.createLevel('Level 5', 3, 4, 0, 0, App.createArr(house, 12), level6.level)
+});
+let level6 = new Level(App.createArr(house, 14), () => {
+    App.createLevel('Level 6', 3, 4, 1, 2, App.createArr(house, 14), level7.level)
+});
+let level7 = new Level(App.createArr(house, 16), () => {
+    App.createLevel('Level 7', 4, 4, 0, 0, App.createArr(house, 16), EventHandler.onClickBackToMenu)
+})
 
 ////////////////////////
 $(() => {
-    createMenu();
+    UI.createMenu();
 })
